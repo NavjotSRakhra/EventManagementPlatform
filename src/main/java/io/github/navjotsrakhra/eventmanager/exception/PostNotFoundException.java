e1e7ea1862a324d02bc3583e3c9c671a1224a74b
Navjot Singh Rakhra <45345158+NavjotSRakhra@users.noreply.github.com>
2023-09-16 12:38:38 +0530

    Added documentation

--- src/main/java/io/github/navjotsrakhra/eventmanager/exception/PostNotFoundException.java
+++ src/main/java/io/github/navjotsrakhra/eventmanager/exception/PostNotFoundException.java
@@ -4,7 +4,13 @@
 
 package io.github.navjotsrakhra.eventmanager.exception;
 
+/**
+ * The PostNotFoundException class is a custom exception class that is thrown when a post is not found.
+ */
 public class PostNotFoundException extends Exception {
+    /**
+     * Constructs a new PostNotFoundException with the specified detail message.
+     */
     public PostNotFoundException() {
         super("Post not found");
     }
