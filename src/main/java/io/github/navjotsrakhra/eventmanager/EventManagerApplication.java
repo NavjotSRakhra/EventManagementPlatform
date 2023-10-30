2cabc5ce3125bd88256ea04b75f5f9e18c441ce3
Navjot Singh Rakhra <45345158+NavjotSRakhra@users.noreply.github.com>
2023-10-30 20:12:28 +0530

    Removed @TypeHints, which work with Spring 2.x

--- src/main/java/io/github/navjotsrakhra/eventmanager/EventManagerApplication.java
+++ src/main/java/io/github/navjotsrakhra/eventmanager/EventManagerApplication.java
@@ -4,16 +4,13 @@
 
 package io.github.navjotsrakhra.eventmanager;
 
-import org.hibernate.dialect.PostgreSQLDialect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.nativex.hint.TypeHint;
 
 /**
  * The EventManagerApplication class is the main class of the EventManager application.
  */
 @SpringBootApplication
-@TypeHint(types = PostgreSQLDialect.class, typeNames = "org.hibernate.dialect.PostgreSQLDialect")
 public class EventManagerApplication {
 
     public static void main(String[] args) {
